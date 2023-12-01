c9d0ee73d6854e369ad560132f4593028d354260
aanakamo <aanakamo@ucsc.edu>
2023-11-30 21:31:15 -0800

    testing orthofinder on example data

--- scripts/orthofinder_run.sh
+++ scripts/orthofinder_run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-#SBATCH --partition=256×44               # Partition/queue to run on
+#SBATCH --partition=128x24               # Partition/queue to run on
 #SBATCH --time=7-00:00:00                # Max time for job to run
 #SBATCH --job-name=orthofinder           # Name for job (shows when running squeue)
 #SBATCH --mail-type=ALL                  # Mail events(NONE,BEGIN,END,FAIL,ALL)
 #SBATCH --mail-user=aanakamo@ucsc.edu    # Where to send mail
 #SBATCH --ntasks=1                       # Number of tasks to run
-#SBATCH --cpus-per-task=44               # Number of CPU cores to use per task
-#SBATCH --mem=256G                       # Ammount of RAM to allocate for the task
+#SBATCH --cpus-per-task=24               # Number of CPU cores to use per task
+#SBATCH --mem=128G                       # Ammount of RAM to allocate for the task
 #SBATCH --output=slurm_%j.out            # Standard output and error log
 #SBATCH --error=slurm_%j.err             # Standard output and error log
 #SBATCH --no-requeue                     # don't requeue the job upon NODE_FAIL
