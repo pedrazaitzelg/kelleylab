3934b12bc03252ff476c3fde54f9d501a0df3301
aanakamo <aanakamo@ucsc.edu>
2023-11-30 21:29:15 -0800

    testing orthofinder on example data

--- scripts/orthofinder_run.sh
+++ scripts/orthofinder_run.sh
@@ -31,5 +31,6 @@
 
 cd /hb/groups/kelley_lab/anne/hibernation/orthofinder_run
 source activate /hb/home/aanakamo/.conda/envs/orthofinder
-orthofinder -f orthofinder_in -t 24 -a 5 -M msa -A mafft -T fasttree -o orthofinder_out -S diamond_ultra_sens
+orthofinder -f ExampleData -t 44 -a 5 -M msa -A mafft -T fasttree -o ExampleData_out -S diamond_ultra_sens
+#orthofinder -f orthofinder_in -t 44 -a 5 -M msa -A mafft -T fasttree -o orthofinder_out -S diamond_ultra_sens
 conda deactivate
