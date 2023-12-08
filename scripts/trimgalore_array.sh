2822174f69ada010691a99128ca1771413c41bf1
aanakamo <aanakamo@ucsc.edu>
2023-12-08 14:05:53 -0800

    trimgalore mem

--- scripts/trimgalore_array.sh
+++ scripts/trimgalore_array.sh
@@ -8,7 +8,7 @@
 #SBATCH --ntasks=1                       # Number of tasks to run
 #SBATCH --cpus-per-task=1                # Number of CPU cores to use per task
 #SBATCH --nodes=1                        # Number of nodes to use
-#SBATCH --mem=10G                        # Ammount of RAM to allocate for the task
+#SBATCH --mem=30G                        # Ammount of RAM to allocate for the task
 #SBATCH --output=slurm_%j.out            # Standard output and error log
 #SBATCH --error=slurm_%j.err             # Standard output and error log
 #SBATCH --no-requeue                     # don't requeue the job upon NODE_FAIL
