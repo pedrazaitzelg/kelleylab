001e64de16e0deb98641c82bd582ebfc7259740b
aanakamo <aanakamo@ucsc.edu>
2023-12-09 12:59:15 -0800

    running trimgalore in scratch

--- scripts/trimgalore_array.sh
+++ scripts/trimgalore_array.sh
@@ -12,7 +12,7 @@
 #SBATCH --output=slurm_%j.out            # Standard output and error log
 #SBATCH --error=slurm_%j.err             # Standard output and error log
 #SBATCH --no-requeue                     # don't requeue the job upon NODE_FAIL
-#SBATCH --array=[1-187]%1                # array job
+#SBATCH --array=[1-187]                  # array job
 
 ### for paralellizing each trim_galore run for SRA samples into a job array
 
