7e301a9b0d3b7945ed56d6c8011817d414c33dc1
aanakamo <aanakamo@ucsc.edu>
2023-12-08 16:00:55 -0800

    update trim script

--- scripts/trimgalore_array.sh
+++ scripts/trimgalore_array.sh
@@ -6,11 +6,11 @@
 #SBATCH --mail-type=ALL                  # Mail events(NONE,BEGIN,END,FAIL,ALL)
 #SBATCH --mail-user=aanakamo@ucsc.edu    # Where to send mail
 #SBATCH --ntasks=1                       # Number of tasks to run
-#SBATCH --cpus-per-task=1                # Number of CPU cores to use per task
+#SBATCH --cpus-per-task=9                # Number of CPU cores to use per task
 #SBATCH --nodes=1                        # Number of nodes to use
-#SBATCH --mem=30G                        # Ammount of RAM to allocate for the task
-#SBATCH --output=slurm_%j.out            # Standard output and error log
-#SBATCH --error=slurm_%j.err             # Standard output and error log
+#SBATCH --mem=8G                         # Ammount of RAM to allocate for the task
+#SBATCH --output=/hb/groups/kelley_lab/anne/slurm_out/slurm_%j.out            # Standard output and error log
+#SBATCH --error=/hb/groups/kelley_lab/anne/slurm_out/slurm_%j.err             # Standard output and error log
 #SBATCH --no-requeue                     # don't requeue the job upon NODE_FAIL
 #SBATCH --array=[1-187]                  # array job
 
@@ -32,7 +32,7 @@ mkdir -p ${species}/${tissue}/trimgalore
 
 module load trimgalore
 
-trim_galore --paired -q 20 --fastqc --fastqc_args "--nogroup --outdir ${species}/${tissue}/fastqc" \
+trim_galore --cores 2 --paired -q 20 --fastqc --fastqc_args "--nogroup --outdir ${species}/${tissue}/fastqc" \
             --stringency 5 --illumina --length 50 -o ${species}/${tissue}/trimgalore --clip_R1 8 --clip_R2 8 \
             ${sra_path}/${sra_acc}_pass_1.fastq ${sra_path}/${sra_acc}_pass_2.fastq
 
