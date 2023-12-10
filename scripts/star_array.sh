b552b2e8f984dca8e24dcac5bc764f5fce3a96ca
aanakamo <aanakamo@ucsc.edu>
2023-12-10 12:06:34 -0800

    star mapping script

--- scripts/star_array.sh
+++ scripts/star_array.sh
@@ -12,13 +12,14 @@
 #SBATCH --output=slurm_%j.out            # Standard output and error log
 #SBATCH --error=slurm_%j.err             # Standard output and error log
 #SBATCH --no-requeue                     # don't requeue the job upon NODE_FAIL
-#SBATCH --array=[1-187]                  # array job
+#SBATCH --array=[1-186]                  # array job
 
 ### for paralellizing each star run for SRA samples into a job array
 
-cd /hb/groups/kelley_lab/anne/hibernation/star_out
+#cd /hb/groups/kelley_lab/anne/hibernation/star_out
+cd /hb/scratch/aanakamo/kelleylab_rotation/star_tmp
 
-LINE=$(sed -n "${SLURM_ARRAY_TASK_ID}"p ../data/transcriptomic/species_tissue_sra_state.txt)
+LINE=$(sed -n "${SLURM_ARRAY_TASK_ID}"p /hb/scratch/aanakamo/kelleylab_rotation/transcriptomic_data_tmp/species_tissue_sra_state.txt)
 species=$(echo ${LINE} | awk '{ print $1; }')
 tissue=$(echo ${LINE} | awk '{ print $2; }')
 sra_acc=$(echo ${LINE} | awk '{ print $3; }')
@@ -28,13 +29,20 @@ echo "running STAR for sra sample: ${sra_acc} (${species}, ${tissue}, ${state})"
 
 genome_dir=../data/genomic/${species}
 fna=$(basename ${genome_dir}/GCF_*_genomic.fna)
-trimmed_dir=../trimgalore_out/${species}/${tissue}/trimgalore
+#trimmed_dir=../trimgalore_out/${species}/${tissue}/trimgalore
+trimmed_dir=../trimgalore_tmp/${species}/${tissue}/trimgalore
 mkdir -p ${species}/${tissue}
 
 # Index genome for use with STAR
 STAR --runMode genomeGenerate --runThreadN 8 --genomeDir ${species}/${tissue} --genomeFastaFiles ${genome_dir}/${fna} --sjdbGTFfile ${genome_dir}/genomic.gff
 
 # Map Reads
-STAR --genomeDir ${species}/${tissue} --runThreadN 8 --outFilterMultimapNmax 1 --twopassMode Basic --sjdbGTFfile ${genome_dir}/genomic.gff \
-        --readFilesCommand zcat --outSAMtype BAM SortedByCoordinate --outFileNamePrefix ./${species}/${tissue}/${sra_acc}_ \
-        --readFilesIn ${trimmed_dir}/${sra_acc}_pass_1_val_1.fq ${trimmed_dir}/${sra_acc}_pass_2_val_2.fq
+if [ "${tissue}" == "wing" ]; then
+        STAR --genomeDir ${species}/${tissue} --runThreadN 8 --outFilterMultimapNmax 1 --twopassMode Basic --sjdbGTFfile ${genome_dir}/genomic.gff \
+                --readFilesCommand zcat --outSAMtype BAM SortedByCoordinate --outFileNamePrefix ./${species}/${tissue}/${sra_acc}_ \
+                --readFilesIn ${trimmed_dir}/${sra_acc}_pass_trimmed.fq.gz
+else
+        STAR --genomeDir ${species}/${tissue} --runThreadN 8 --outFilterMultimapNmax 1 --twopassMode Basic --sjdbGTFfile ${genome_dir}/genomic.gff \
+                --readFilesCommand zcat --outSAMtype BAM SortedByCoordinate --outFileNamePrefix ./${species}/${tissue}/${sra_acc}_ \
+                --readFilesIn ${trimmed_dir}/${sra_acc}_pass_1_val_1.fq ${trimmed_dir}/${sra_acc}_pass_2_val_2.fq
+fi
