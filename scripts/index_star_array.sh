25171b7e459d6407ffdab7f2a97add74ee904a3d
aanakamo <aanakamo@ucsc.edu>
2023-12-10 13:10:54 -0800

    index star script

--- scripts/index_star_array.sh
+++ scripts/index_star_array.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 #SBATCH --partition=128x24               # Partition/queue to run on
-#SBATCH --time=1-00:00:00                # Max time for job to run
+#SBATCH --time=2-00:00:00                # Max time for job to run
 #SBATCH --job-name=star                  # Name for job (shows when running squeue)
 #SBATCH --mail-type=ALL                  # Mail events(NONE,BEGIN,END,FAIL,ALL)
 #SBATCH --mail-user=aanakamo@ucsc.edu    # Where to send mail
 #SBATCH --ntasks=1                       # Number of tasks to run
-#SBATCH --cpus-per-task=8                # Number of CPU cores to use per task
+#SBATCH --cpus-per-task=6                # Number of CPU cores to use per task
 #SBATCH --nodes=1                        # Number of nodes to use
-#SBATCH --mem=10G                        # Ammount of RAM to allocate for the task
+#SBATCH --mem=15G                        # Ammount of RAM to allocate for the task
 #SBATCH --output=slurm_%j.out            # Standard output and error log
 #SBATCH --error=slurm_%j.err             # Standard output and error log
 #SBATCH --no-requeue                     # don't requeue the job upon NODE_FAIL
@@ -31,4 +31,4 @@ mkdir -p ${species}
 cd ${species}
 
 # Index genome for use with STAR
-STAR --runMode genomeGenerate --runThreadN 8 --genomeDir . --genomeFastaFiles ${genome_dir}/${fna} --sjdbGTFfile ${genome_dir}/genomic.gff
+STAR --runMode genomeGenerate --runThreadN 6 --genomeDir . --genomeFastaFiles ${genome_dir}/${fna} --sjdbGTFfile ${genome_dir}/genomic.gff
