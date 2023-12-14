e5bc6f5f695e16437f272185bd2cdcd23add94c1
aanakamo <aanakamo@ucsc.edu>
2023-12-14 13:22:50 -0800

    protein_id to geneID

--- scripts/og_proteinid.py
+++ scripts/og_proteinid.py
@@ -11,6 +11,7 @@ with open(gff_file, 'r') as gff:
         lst = line.rstrip().split('\t')
         if len(lst) == 9 and lst[2] == "CDS":
             full_att = lst[8].split(';')
+            print(full_att)
             protein_id = [x for x in full_att if "protein_id" in x][0].split('=')[1]
             GeneID = [x for x in full_att if "GeneID" in x][0].split(':')[1]
             if pID_geneID.get(protein_id):
