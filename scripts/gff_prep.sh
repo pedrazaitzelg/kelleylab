cd63f959797b164a4938d92c1ca735f5c76b8939
aanakamo <aanakamo@ucsc.edu>
2023-12-14 13:15:26 -0800

    protein_id to geneID

--- scripts/gff_prep.sh
+++ scripts/gff_prep.sh
@@ -32,12 +32,15 @@ cd ${species}
 ### parse orthogroup tsv file, output has cols: "protein_id", "Orthogroup"
 python2 ~/kelleylab_rotation/scripts/blairs_gff_scripts/parseOrthogroups.py ${og_dir}/Orthogroups.tsv ${species}.prepped ${col_num}
 ### make a mapping between "protein_id" and "GeneID"
+python ~/kelleylab_rotation/scripts/og_proteinid.py ${gff_file} ${og_dir}/Orthogroups.${species}.tsv
 
-### add orthogroup info to the gff file
-python2 ~/kelleylab_rotation/scripts/blairs_gff_scripts/annotateGFF.py ${gff_file} ${og_dir}/Orthogroups.${species}.tsv ${species}.OG.gff
+### add "protein_id", "Orthogroup" info to the gff file
+python2 ~/kelleylab_rotation/scripts/blairs_gff_scripts/annotateGFF.py ${gff_file} ${og_dir}/Orthogroups.${species}.protein_id.tsv ${species}.OG.gff
 
 ### convert GFF to GTF w/ AGAT
+module load agat
 
+module unload agat
 
 ### Convert GTF to SAF
 
---
+++ scripts/og_proteinid.py
@@ -0,0 +1,29 @@
+import sys
+
+gff_file = sys.argv[1]
+og_tsv = sys.argv[2]
+
+outfile = outfile = og_tsv[:-3] + '.protein_id.tsv'
+
+pID_geneID = {}
+with open(gff_file, 'r') as gff:
+    for line in gff:
+        lst = line.rstrip().split('\t')
+        if lst[2] == "CDS":
+            full_att = line[8].split(';')
+            protein_id = [x for x in full_att if "protein_id" in x][0].split('=')[1]
+            GeneID = [x for x in full_att if "GeneID" in x][0].split(':')[1]
+            if pID_geneID.get(protein_id):
+                if pID_geneID[protein_id] != GeneID:
+                    print("Inconsistent protein_id GeneID pair: " + ", ".join([protein_id, pID_geneID[protein_id], GeneID]))
+            else:
+                pID_geneID[protein_id] = GeneID
+
+with open(og_tsv, 'r') as og:
+    with open(outfile, 'w') as out:
+        for line in og:
+            lst = line.rstrip().split()
+            protein_id = lst[0]
+            Orthogroup = lst[1]
+            geneID = pID_geneID[protein_id]
+            out.write("\t".join([geneID, Orthogroup, protein_id] + "\n"))
