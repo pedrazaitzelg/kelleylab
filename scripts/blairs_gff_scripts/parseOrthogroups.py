e2c15bc8a8fa0d22d3962039eafdb4885367c3bb
aanakamo <aanakamo@ucsc.edu>
2023-12-15 12:21:05 -0800

    prep gff

--- scripts/blairs_gff_scripts/parseOrthogroups.py
+++ scripts/blairs_gff_scripts/parseOrthogroups.py
@@ -40,6 +40,6 @@ with open(orthogroups) as a:
 with open(outfile,'w') as out:
     print >> out, '\t'.join(['protein_id','Orthogroup'])    ## changed "Parent" to "protein_id"
     for entry in gene_to_ortho:
-        output_entry = [entry.rstrip("_" + species_name),gene_to_ortho[entry]]  ## changed to remove species name from end of protein_id
+        output_entry = [entry.removesuffix("_" + species_name),gene_to_ortho[entry]]  ## changed to remove species name from end of protein_id
         print >> out, '\t'.join(output_entry)
 
