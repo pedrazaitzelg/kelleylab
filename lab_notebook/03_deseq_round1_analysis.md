4b6f7f651b3db210db84202a4114a167644f4c05
aanakamo <aanakamo@ucsc.edu>
2024-01-09 08:36:47 -0800

    finding 1-1 DE orthologs

--- lab_notebook/03_deseq_round1_analysis.md
+++ lab_notebook/03_deseq_round1_analysis.md
@@ -16,3 +16,27 @@ Notes:
 ### Grouped by tissue
 ![Upset plots](supporting_images/upset_plots.pdf)
 
+## DE SCOs
+Table showing the total DE orthogroups and DE SCOs for each species,tissue
+| species | tissue | DE_OG_count | DE_SCO_count |
+| :---------------- | :------ | ----: | ----: |
+| 13_lined_ground_squirrel | cerebrum | 2613 | 99 |
+| 13_lined_ground_squirrel | hypothalamus | 4690 | 228 |
+| 13_lined_ground_squirrel | medulla | 4836 | 213 |
+| arctic_ground_squirrel | muscle | 964 | 54 |
+| black_bear | bone | 8222 | 361 |
+| brandts_bat | brain | 306 | 15 |
+| brandts_bat | kidney | 1912 | 81 |
+| brandts_bat | liver | 1907 | 91 |
+| brown_bear | adipose | 6179 | 275 |
+| brown_bear | liver | 4573 | 178 |
+| brown_bear | muscle | 1678 | 71 |
+| djungarian_hamster | blood | 174 | 7 |
+| dwarf_lemur | white_adipose | 1221 | 61 |
+| greater_horseshoe_bat | intestine | 1243 | 57 |
+| monito_del_monte | brain | 179 | 11 |
+| monito_del_monte | liver | 40 | 2 |
+| monito_del_monte | muscle | 9 | 2 |
+| syrian_hamster | white_adipose | 4260 | 176 |
+
+
---
+++ scripts/filter_pav.py
@@ -0,0 +1,46 @@
+import sys
+import os
+
+os.chdir("/Users/annenakamoto/ROTATION2")   ## locally
+sco_file = "Orthogroups/Orthogroups_SingleCopyOrthologues.txt"
+og_file = "Orthogroups/Orthogroups.txt"
+de_genes_file = "DESEQ2/DE_GENES/ALL.DEgenes.tsv"
+
+### determine number of 1-1 orthologs (SCOs) in the final dataset
+SCO = {}
+with open(sco_file, 'r') as sco:
+    for line in sco:
+        og = line.strip()
+        SCO[og] = 1
+
+SP_TS_SCO = {}
+with open(de_genes_file, 'r') as de:
+    for line in de:
+        lst = line.strip().split()
+        if lst[0] != "species":
+            species = lst[0]
+            tissue = lst[1]
+            og = lst[4]
+            sp_ts = (species, tissue)
+            if SP_TS_SCO.get(sp_ts):
+                SP_TS_SCO[sp_ts][0].append(og)
+            else:
+                SP_TS_SCO[sp_ts] = [[og], []]
+            if SCO.get(og):
+                    SP_TS_SCO[sp_ts][1].append(og)
+
+with open("species_tissue_de_scos.tbl.txt", 'w') as out:
+    out.write('\t'.join(["species", "tissue", "DE_OG_count", "DE_SCO_count"]) + '\n')
+    for sp_ts, ogs in sorted(SP_TS_SCO.items(), key=lambda x: x[0][0]):
+        out.write('\t'.join([sp_ts[0], sp_ts[1], str(len(ogs[0])), str(len(ogs[1]))]) + '\n')
+
+with open("species_tissue_de_scos.lst.txt", 'w') as out:
+    out.write('\t'.join(["species", "tissue", "DE_OGs", "DE_SCOs"]) + '\n')
+    for sp_ts, ogs in sorted(SP_TS_SCO.items(), key=lambda x: x[0][0]):
+        out.write('\t'.join([sp_ts[0], sp_ts[1], ";".join(ogs[0]), ";".join(ogs[1])]) + '\n')
+
+### filter out DE genes that are only present in one species (PAV)
+### record how many there are
+
+
+
