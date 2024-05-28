8e2333a7dad87ad444eef43d089433ba07373828
pedrazaitzelg <155600640+pedrazaitzelg@users.noreply.github.com>
2024-05-28 15:38:54 -0700

    Update index_star_array.sh

--- itzel_notebook/scripts/index_star_array.sh
+++ itzel_notebook/scripts/index_star_array.sh
@@ -21,20 +21,20 @@
 ### done once per genome ###
 
 
-cd /hb/groups/kelley_training/itzel/data/genome/lesser_dwarf_lemur/star_out
-#cd /hb/scratch/itzel/kelleylab/star_tmp
+cd /hb/groups/kelley_training/itzel/data/genome/hibernation    #directory for data to run in
 
 module load star
 
-LINE=$(sed -n "${SLURM_ARRAY_TASK_ID}"p /hb/groups/kelley_training/itzel/data/genome/lesser_dwarf_lemur/species_gcf.txt)
+LINE=$(sed -n "${SLURM_ARRAY_TASK_ID}"p /hb/groups/kelley_training/itzel/data/genome/lesser_dwarf_lemur/species_gcf.txt)    #location for species_gcf.txt files
 species=$(echo ${LINE} | awk '{ print $1; }')
 
 echo "running STAR indexing for: ${species}"
 
-genome_dir=/hb/groups/kelley_training/itzel/data/genome/${species}
-fna=GCF_000165445.2_Mmur_3.0_genomic.fna
-mkdir -p lesser_dwarf_lemur
-cd ${species}
+genome_dir=/hb/groups/kelley_training/itzel/data/genome/${species}    #location of genomic data
+fna=$(basename ${genome_dir}/GCF_*_genomic.fna)      #location of .fna files  |   typically GCF files bute in not RefSeq then edit to GCA
+mkdir -p ${species}      #-p will make parent directory if not already made
+cd ${species}      #goes into that directory
 
 # Index genome for use with STAR (one genome needed more RAM, which is why the --limitGenomeGenerateRAM option is used)
+# If only using GCA (GenBank) that has no annotations, delete line for .gff
 STAR --runMode genomeGenerate --runThreadN 8 --genomeDir . --genomeFastaFiles ${genome_dir}/${fna} --sjdbGTFfile ${genome_dir}/genomic.gff --limitGenomeGenerateRAM 123560700863
