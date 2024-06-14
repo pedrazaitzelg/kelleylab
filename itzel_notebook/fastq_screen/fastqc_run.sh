b63df5ba0241aae0fd638252fb947021e6717a3a
pedrazaitzelg <155600640+pedrazaitzelg@users.noreply.github.com>
2024-06-13 22:58:02 -0700

    Create fastqc_run.sh

---
+++ itzel_notebook/fastq_screen/fastqc_run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#SBATCH --partition=128x24               # Partition/queue to run on
+#SBATCH --time=1-00:00:00                # Max time for job to run
+#SBATCH --job-name=fastq_run                # Name for job (shows when running squeue)
+#SBATCH --mail-type=ALL                  # Mail events(NONE,BEGIN,END,FAIL,ALL)
+#SBATCH --mail-user=igpedraz@ucsc.edu    # Where to send mail
+#SBATCH --ntasks=1                       # Number of tasks to run
+#SBATCH --cpus-per-task=8                # Number of CPU cores to use per task
+#SBATCH --nodes=1                        # Number of nodes to use
+#SBATCH --mem=10G                        # Ammount of RAM to allocate for the task
+#SBATCH --output=slurm_%j.out            # Standard output and error log
+#SBATCH --error=slurm_%j.err             # Standard output and error log
+#SBATCH --no-requeue                     # don't requeue the job upon NODE_FAIL
+#SBATCH --array=[1-22]                  # array job
+
+
+## for running each trimmed fastqc file of sra data against reference genomes
+
+## location of fastqc program file followed by lcoation of .fq.gz files to be ran 
+
+../FastQ-Screen-0.15.3/fastq_screen ../transcriptomic/lesser_dwarf_lemur/SRR16*_pass_*_val_*.fq.gz
