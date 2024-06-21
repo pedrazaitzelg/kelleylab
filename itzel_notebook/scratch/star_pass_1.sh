3fb815b250c270289a0d856a97d19d2c77307f66
pedrazaitzelg <155600640+pedrazaitzelg@users.noreply.github.com>
2024-06-20 17:00:57 -0700

    Create star_pass_1.sh

---
+++ itzel_notebook/scratch/star_pass_1.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#SBATCH --partition=256x44               # Partition/queue to run on (usually 128x24)
+#SBATCH --time=2-00:00:00                # Max time for job to run
+#SBATCH --job-name=star                  # Name for job (shows when running squeue)
+#SBATCH --mail-type=ALL                  # Mail events(NONE,BEGIN,END,FAIL,ALL)
+#SBATCH --mail-user=igpedraz@ucsc.edu    # Where to send mail
+#SBATCH --output=star.out
+#SBATCH --error=star.err
+#SBATCH --ntasks=1                       # Number of tasks to run
+#SBATCH --cpus-per-task=4                # Number of CPU cores to use per task
+#SBATCH --nodes=1                        # Number of nodes to use
+#SBATCH --mem=40G                        # Ammount of RAM to allocate for the task
+#SBATCH --output=slurm_%j.out            # Standard output and error log
+#SBATCH --error=slurm_%j.err             # Standard output and error log
+#SBATCH --no-requeue                     # don't requeue the job upon NODE_FAIL
+
+### for paralellizing each star run for SRA samples into a job array
+
+### single star run for 1 pass of 1 sample of dwarf lemur
+
+cd /hb/groups/kelley_training/itzel/data/transcriptomic
+
+module load star
+
+genome_dir=/hb/groups/kelley_training/itzel/anne/hibernation/data/genomic/dwarf_lemur #location of genome
+fna=/hb/groups/kelley_training/itzel/anne/hibernation/data/genomic/dwarf_lemur/GCF_000165445.2_Mmur_3.0_genomic.fna     #location of fna file
+trimmed_dir=/hb/groups/kelley_training/itzel/anne/hibernation/trimgalore_out/dwarf_lemur/white_adipose/trimgalore    #location of trimmed directory
+mkdir -p ${species}/${tissue}/${sra_acc}    
+cd ${species}/${tissue}/${sra_acc}
+
+# Map Reads
+if [ -f ${sra_acc}_Log.final.out ]; then
+    echo "already finished"
+else
+    STAR --genomeDir ${genome_dir} --runThreadN 4 --outFilterMultimapNmax 1 --twopassMode Basic --sjdbGTFfile ${genome_dir}/genomic.gff \
+        --readFilesCommand zcat --outSAMtype BAM SortedByCoordinate --outFileNamePrefix ./${sra_acc}_ \
+        --readFilesIn ${trimmed_dir}/SRR5993015_pass_1_val_1.fq.gz  
+fi
