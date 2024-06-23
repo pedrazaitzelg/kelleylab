c24aae7bbc2629e147aef1315c973e6f6e1ca35f
pedrazaitzelg <155600640+pedrazaitzelg@users.noreply.github.com>
2024-06-23 14:24:14 -0700

    Update bbmerge_array.sh

--- itzel_notebook/scripts/bbmerge_array.sh
+++ itzel_notebook/scripts/bbmerge_array.sh
@@ -5,16 +5,17 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=igpedraz@ucsc.edu
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1 
-#SBATCH --nodes=1 
+#SBATCH --cpus-per-task=1
+#SBATCH --nodes=1
 #SBATCH --mem=10G
 #SBATCH --output=bbmerge_run.out
 #SBATCH --error=bbmerge_run.err
-#SBATCH --no-requeue 
+#SBATCH --no-requeue
 
 
 # module to run bbmerge located under bbtools
 module load hb hb-gnu bbtools/bbtools-39.01
 
 # this line will run the program for the 2 reads
-bbmerge.sh in1=read1.fq in2=read2.fq out=merged.fq
+# in1 = and in2 = set to locations of reads
+bbmerge.sh in1=/hb/groups/kelley_training/itzel/data/anne/data/transcriptomic/dwarf_lemur_crossleyi/white_adipose/SRR5993015_pass_1.fastq.gz in2=/hb/groups/kelley_training/itzel/data/anne/data/transcriptomic/dwarf_lemur_crossleyi/white_adipose/SRR5993015_pass_2.fastq.gz out=SRR5993015_merged.fq
