117818b37f5155cbcfa29df7d302c1dcdd9e671d
pedrazaitzelg <155600640+pedrazaitzelg@users.noreply.github.com>
2024-06-26 10:47:07 -0700

    Update bbmerge_array.sh

--- itzel_notebook/scripts/bbmerge_array.sh
+++ itzel_notebook/scripts/bbmerge_array.sh
@@ -11,7 +11,7 @@
 #SBATCH --output=bbmerge_run.out
 #SBATCH --error=bbmerge_run.err
 #SBATCH --no-requeue
-#SBATCH --array=[1-16]
+#SBATCH --array=[3-14]
 
 
 # module to run bbmerge located under bbtools
