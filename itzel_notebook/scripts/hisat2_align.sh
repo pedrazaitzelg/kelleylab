ab8b98c870eee090f84034916817c56f04d49bcc
pedrazaitzelg <155600640+pedrazaitzelg@users.noreply.github.com>
2024-08-13 12:35:43 -0700

    Update hisat2_align.sh

--- itzel_notebook/scripts/hisat2_align.sh
+++ itzel_notebook/scripts/hisat2_align.sh
@@ -14,7 +14,7 @@
 #SBATCH --no-requeue                     # don't requeue the job upon NODE_FAIL
 
 #indexing reference genome for hisat2
-cd /hb/groups/kelley_training/itzel/data/hisat2
+cd /hb/groups/kelley_training/itzel/data/hisat2/test_run
 
 #load module
 module load hisat/2.1.0
@@ -22,4 +22,4 @@ module load hisat/2.1.0
 #run hisat alignment
 # hisat2 [options]* -x <hisat2-idx> {-1 <m1> -2 <m2> | -U <r> | --sra-acc <SRA accession number>} [-S <hit>]
 # -q indicates using fastq files
-hisat2 -q -x index_genomes/dwarf_lemur -1 transcriptomic_data/dwarf_lemur/SRR5993015_pass_1.fastq.gz  -2 transcriptomic_data/dwarf_lemur/white_adipose/SRR5993015_pass_2.fastq.gz -S SRR5993015.sam
+hisat2 -q -x ../index_genomes/dwarf_lemur/ -1 ../transcriptomic_data/dwarf_lemur_crossleyi/white_adipose/SRR5993015_pass_1.fastq.gz  -2 ../transcriptomic_data/dwarf_lemur_crossleyi/white_adipose/SRR5993015_pass_2.fastq.gz -S SRR5993015.sam
