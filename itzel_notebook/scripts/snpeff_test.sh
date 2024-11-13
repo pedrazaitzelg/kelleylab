f0d421f924a4f7b21f3765cb456b0c1b0d0d286e
pedrazaitzelg <155600640+pedrazaitzelg@users.noreply.github.com>
2024-11-12 17:55:11 -0700

    Update snpeff_test.sh

--- itzel_notebook/scripts/snpeff_test.sh
+++ itzel_notebook/scripts/snpeff_test.sh
@@ -1,4 +1,24 @@
+#!/bin/bash
 
+#SBATCH --partition=128x24               # Partition/queue to run on
+#SBATCH --time=2-00:00:00                # Max time for job to run
+#SBATCH --job-name=snpeff_run           # Name for job (shows when running squeue)
+#SBATCH --mail-type=ALL                  # Mail events(NONE,BEGIN,END,FAIL,ALL)
+#SBATCH --mail-user=igpedraz@ucsc.edu    # Where to send mail
+#SBATCH --ntasks=1                       # Number of tasks to run
+#SBATCH --cpus-per-task=8                # Number of CPU cores to use per task
+#SBATCH --nodes=1                        # Number of nodes to use
+#SBATCH --mem=15G                        # Ammount of RAM to allocate for the task
+#SBATCH --output=slurm_%j.out            # Standard output and error log
+#SBATCH --error=slurm_%j.err             # Standard output and error log
+#SBATCH --no-requeue                     # don't requeue the job upon NODE_FAIL
 
+### load module ###
+module load miniconda3
+conda activate snpeff
 
-java -Xmx8g -jar snpEff.jar GRCh37.75 examples/test.chr22.vcf > test.chr22.ann.vcf
+### run vcf file through snpeff to annotate variations ###
+java -Xmx8g -jar snpEff.jar UrsArc2.0 allgenes_intersect.vcf > all_genes_ann.vcf
+
+### deactivate snpeff ###
+conda deactivate 
