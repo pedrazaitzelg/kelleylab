eb078dfb6ee6cff6c23e36c6f7ea9308b42cad07
pedrazaitzelg <155600640+pedrazaitzelg@users.noreply.github.com>
2024-11-18 14:46:12 -0700

    Update and rename snpeff_test.sh to snpeff_annotate.sh

--- itzel_notebook/scripts/snpeff_annotate.sh
+++ itzel_notebook/scripts/snpeff_annotate.sh
@@ -15,10 +15,20 @@
 
 ### load module ###
 module load miniconda3
-conda activate snpeff_env
+conda activate snpeff
+
+### locate directory with snpeff location ###
+cd /hb/groups/kelley_training/itzel/population_bears_proj24/snpEff_out
+
+### create annotated file destination ###
+touch all_genes_ann.vcf
+
+###cd /hb/home/igpedraz/.conda/envs/snpeff
 
 ### run vcf file through snpeff to annotate variations ###
-snpEff UrsArc2.0 allgenes_intersect.vcf > all_genes_ann.vcf
+snpEff UrsArc2.0 \
+/hb/groups/kelley_training/itzel/population_bears_proj24/bedtools_out/allgenes_intersect.vcf \
+> /hb/groups/kelley_training/itzel/population_bears_proj24/snpEff_out/all_genes_ann.vcf
 
 ### deactivate snpeff ###
-conda deactivate 
+conda deactivate
