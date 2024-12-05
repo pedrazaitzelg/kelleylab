7343ed2c8ea42f5fc560429d7f099957d5ec60f8
pedrazaitzelg <155600640+pedrazaitzelg@users.noreply.github.com>
2024-12-05 16:52:42 -0700

    Update agat.sh

--- itzel_notebook/scripts/agat.sh
+++ itzel_notebook/scripts/agat.sh
@@ -22,4 +22,11 @@ conda activate agat_env
 
 # Extract cds regions from gff/gtf file
 # Replace infile.gff and infile.fasta with GFF/GTF file locations
-agat_sp_extract_sequences.pl -g genomic.gff -f GCF_023065955.2_UrsArc2.0_genomic.fasta -t cds -o cds_agat.fa
+
+# Remove # for desirec command
+# Generates cds:
+#agat_sp_extract_sequences.pl -g genomic.gff -f GCF_023065955.2_UrsArc2.0_genomic.fasta -t cds -o cds_agat.fa
+
+# Generates protein files:
+agat_sp_extract_sequences.pl -g genomic.gff -f GCF_023065955.2_UrsArc2.0_genomic.fasta -t cds -p -o protein_agat.fa
+
