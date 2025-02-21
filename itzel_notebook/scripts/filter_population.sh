bf33687407fc449a12535262bef7b934c4507cbb
pedrazaitzelg <155600640+pedrazaitzelg@users.noreply.github.com>
2025-02-21 14:11:47 -0700

    Update filter_population.sh

--- itzel_notebook/scripts/filter_population.sh
+++ itzel_notebook/scripts/filter_population.sh
@@ -12,19 +12,30 @@
 #SBATCH --output=slurm_%j.out            # Standard output and error log
 #SBATCH --error=slurm_%j.err             # Standard output and error log
 #SBATCH --no-requeue                     # don't requeue the job upon NODE_FAIL
-#SBATCH --array=[1-1]                 # array job
 
 
 ## Subset provided populations file into each population
 ## to be used with the larger vcf file for pop-level stats
 
+
 #Set Variables
-bear_in=/hb/groups/kelley_training/itzel/population_bears_proj24/population_stats/bear_pop.txt
+bear_in=/hb/groups/kelley_training/itzel/population_bears_proj24/population_stats/mod_bear_pop.txt
 
 LINE=$(sed -n "${SLURM_ARRAY_TASK_ID}"p $bear_in)
 indiv_id=$(echo ${LINE} | awk '{ print $1; }')
-location=$(echo ${LINE} | awk '{ print $2; }')
+#variable not needed
+#location=$(echo ${LINE} | awk '{ print $2; }')
 sub_loc=$(echo ${LINE} | awk '{ print $3; }')
 
+#create temp file containing location names
+awk '{print $2}' ${bear_in} | sort | uniq > locations.txt
+location_name=$(echo locations.txt | awk '{ print $1; }')
+
 # create subset files
-awk "$2 == ${location}" ${bear_in} > ${location}_indiv.txt
+while read location; do
+    # Create a subset for each unique location
+    awk -v location="$location" '$2 == location' ${bear_in} | awk '{print $1}' > "${location}_indiv.txt"
+done < locations.txt
+
+#remove temp file
+rm locations.txt
