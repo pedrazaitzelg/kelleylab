cda928a6f1087e6b8f2535dd0c760eecf5af6e1d
pedrazaitzelg <155600640+pedrazaitzelg@users.noreply.github.com>
2025-02-21 13:15:17 -0700

    Update filter_population.sh

--- itzel_notebook/scripts/filter_population.sh
+++ itzel_notebook/scripts/filter_population.sh
@@ -12,12 +12,19 @@
 #SBATCH --output=slurm_%j.out            # Standard output and error log
 #SBATCH --error=slurm_%j.err             # Standard output and error log
 #SBATCH --no-requeue                     # don't requeue the job upon NODE_FAIL
+#SBATCH --array=[1-1]                 # array job
+
 
 ## Subset provided populations file into each population
 ## to be used with the larger vcf file for pop-level stats
 
 #Set Variables
 bear_in=/hb/groups/kelley_training/itzel/population_bears_proj24/population_stats/bear_pop.txt
-pop=
 
-awk "$2 == $pop" $bear_in 
+LINE=$(sed -n "${SLURM_ARRAY_TASK_ID}"p $bear_in)
+indiv_id=$(echo ${LINE} | awk '{ print $1; }')
+location=$(echo ${LINE} | awk '{ print $2; }')
+sub_loc=$(echo ${LINE} | awk '{ print $3; }')
+
+# create subset files
+awk "${location}" ${bear_in} > ${location}_indiv.txt
